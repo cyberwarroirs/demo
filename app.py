69b68253400cfbbe7fdfd98988c2b66d5f3a6085
cyberwarroirs <vijaywagh7391@gmail.com>
2025-04-02 14:39:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ def hello_world():
    return 'Hello World'
 
 if __name__ == '__main__':
-   app.run()
+    # Start the main app (this will keep it running)
+    app.run(host="0.0.0.0", port=5000, debug=True)
